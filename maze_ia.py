12b0714e2b07b63d09b7608f858714237fbd0e94
Tai <thaivinhtai@gmail.com>
2019-02-22 11:36:37 +0700

    update

--- maze_ia.py
+++ maze_ia.py
@@ -2,39 +2,41 @@
 from sys import stdin, stdout, stderr
 
 
+# Define Queue data str
 class Queue:
     def __init__(self):
         self.queue = list()
 
+    # Add element to first posotion of queue
     def enqueue(self, elements):
         if elements not in self.queue:
             self.queue.insert(0, elements)
             return True
         return False
 
+    # Take the last element out of queue
     def dequeue(self):
         if len(self.queue) > 0:
             return self.queue.pop()
         return "Queue Empty!"
 
+    # Return the lenght of queue
     def size(self):
         return len(self.queue)
 
+    # Print the queue
     def print_queue(self):
         return self.queue
-
-
-class Resources:
-    def __init__(self):
-        self.coin = "o"
-        self.bonus = "!"
-        self.wall = "*"
 
 
 class Maze:
     def __init__(self):
         self.maze = list()
+        self.wall = "*"
+        self.list_resoueces = ["o", "!"]
+        self.list_resources_pos = list()
 
+    # Get the current Maze's status
     def get_maze(self):
         line = ""
         while line != "\n":
@@ -42,56 +44,71 @@ class Maze:
             self.maze.append(line)
         self.maze.remove(self.maze[0])
 
+    # Get list of current resources position
+    def get_resources_pos():
+        temp_index = -1
+        for y in range(len(self.maze)):
+            for x in range(len(self.maze[i])):
+                if self.maze[i][j] in self.list_resources:
+                    temp_index = temp_index + 1
+                    self.list_resources_pos.append([])
+                    if self.maze[i][j] == "o":
+                        self.list_resoueces_pos[temp_index].append("o")
+                    elif self.maze[i][j] == "!":
+                        self.list_resoueces_pos[temp_index].appen("!")
+                    self.list_resoueces_pos[temp_index].append(y)
+                    self.list_resoueces_pos[temp_index].append(x)
+
 
 class Intelligent_Agent:
-    def __init__(self, name="Tai"):
+    def __init__(self, name="Tai's IA"):
         self.name = name
         self.letter = "A"
         self.posx = -2
         self.posy = -2
 
+    # Introduce IA to VM
     def greeting():
-        stdin.readln()
-        stdin.readln()
+        line = ""
+        while line != "\n":
+            line = stdin.readln()
         stdout.write("I AM {name}\n\n".format(name=self.name))
         letter = stdin.readln()
         stdin.readln()
         self.letter = letter(len[letter] - 1)
         stdout.write("OK\n\n")
 
+    # Figure out the position of IA
     def get_IA_position(self, maze):
-        for i in range(len(maze) - 1):
-            for j in range(len(maze[i]) - 1):
-                if maze[i][j] == self.letter:
-                    self.posx = i
-                    self.posy = j
+        for y in range(len(maze)):
+            for x in range(len(maze[i])):
+                if maze[y][x] == self.letter:
+                    self.posx = x
+                    self.posy = y
                     break
-
-    def get_resources_position(self, maze):
-        for i in range(len(maze) - 1):
-            for j in range(len(maze[i]) - 1):
-                if maze[i][j] == Resources.coin
-                or maze[i][j] == Resources.bonus:
-                    temp_pos = []
-                    temp_pos.append(i)
-                    temp_pos.append(j)
-                    if temp_pos not in self.resources:
-                        self.resources.append(temp_pos)
 
     def move_left():
         stdin.write("MOVE LEFT\n\n")
+        move = [0, -1]
+        return move
 
     def move_right():
         stdin.write("MOVE RIGHT\n\n")
+        move = [0, 1]
+        return move
 
     def move_up():
         stdin.write("MOVE UP\n\n")
+        move = [-1, 0]
+        return move
 
     def move_down():
         stdin.write("MOVE DOWN\n\n")
+        move = [1, 0]
+        return move
 
 
 if __name__ == "main":
-    play = Intelligent_Agent
+    IA = Intelligent_Agent
     play.greeting()
     play.get_maze()
